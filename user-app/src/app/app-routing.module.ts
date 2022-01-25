0405c1990a13a0d4212c3c16b4f08a412b24fe0b
yaron <yaron.kessler41@gmail.com>
2022-01-25 19:50:17 +0200

    basic

--- user-app/src/app/app-routing.module.ts
+++ user-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
