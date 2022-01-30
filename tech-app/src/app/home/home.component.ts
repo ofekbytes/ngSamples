9436e8b0805373ea7171b5943b182c4d82331f08
yaron <yaron.kessler41@gmail.com>
2022-01-30 09:07:29 +0200

    tech-app

--- tech-app/src/app/home/home.component.ts
+++ tech-app/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,12 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  clickCounter: number = 0;
-  name: string = '';
+  // Ticket
+  openTicketDate!: Date;
+  openTicketTime!: Time;
+  closedTicketDate!: Date;
+  closedTicketTime!: Time;
 
+  // User
+  userFirstName: string = '';
+  userLastName: string = '';
+  userPhone1:  string = '' ;
+  userPhone2:  string = '' ;
+
+  // Technician
+  technicianFirstName: string = '';
+  technicianLastName: string = '';
+  technicianPhone1:  string = '' ;
+  technicianPhone2:  string = '' ;
+
+
+  clickCounter: number = 0;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  clearAllFields() {
+    console.log("clearAllFields");
   }
 
   countClick() {
